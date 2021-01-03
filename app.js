37cdc61c1e11f8fa45aa5dd87e385fe07814c92e
Diyor Mukhamedkhanov <d.mukhamedkhanov98@gmail.com>
2021-01-03 22:48:33 +0500

    Geocode module created

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const request = require('request')
+const geocode = require('./utils/geocode.js')
+
 
 // const url = 'http://api.weatherstack.com/current?access_key=88cb06ba50103cb611375faaa1f97483&query=37.8267,-122.4233&units=s'
 
@@ -14,17 +16,9 @@ const request = require('request')
 // })
 
 
-const geocode = "https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoiZGl5b3I5OCIsImEiOiJja2poNHdtb245NWVxMnlsYnM5YmIxa3RyIn0.GTtVHEBp4vdyHpPxLK0wdw&limit=1"
 
-request({ url: geocode, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connet to mapbox service')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location')
-    } else {
-        const longitude = response.body.features[0].center[0]
-        const latitude = response.body.features[0].center[1]
-        console.log("Latitude " + latitude + ". Longitude " + longitude)
-    }
 
+geocode('Boston', (error, data) => {
+    console.log('Error: ', error)
+    console.log('Data: ', data )
 })
